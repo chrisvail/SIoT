e15f682e257d27653830f840d5466cbdb03f861e
chrisvail <40955766+chrisvail@users.noreply.github.com>
2021-12-17 15:00:02 +0000

    Update store_image.py

--- store_image.py
+++ store_image.py
@@ -15,27 +15,4 @@ with open(img_file, "rb") as img:
     s3.Bucket('siotimages').put_object(Key=img_file, Body=img)
 
 os.remove(img_file)
-# find jpg file
-# open file
-# send to s3
-# delete image
 
-
-
-
-
-
-
-
-# s3client = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-
-# data = s3client.get_object(Bucket="siotimages", Key="test.jpg")
-# pprint(data)
-
-# pprint(dir(data["Body"]))
-
-# with open("test_download.jpg", "wb") as img:
-#     img.write(data["Body"].read())
-
-# AKIAUZN3TC2NVAUB6VNI
-# W3sX2NzYcqasEgS8ZYuXbuYzw61xpjE8XW6cP4f4
